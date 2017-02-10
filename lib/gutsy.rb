06dedca5d2b2374619a587eecb07d27926c41b70
Max Fierke <max.fierke@iorahealth.com>
2017-02-10 15:19:25 -0600

    wip: refactor CLI a bit, still needs more to support specifing multiple versions

--- lib/gutsy.rb
+++ lib/gutsy.rb
@@ -5,69 +5,10 @@ require 'erb'
 require 'forwardable'
 require 'json_schema'
 require 'open-uri'
-require 'gutsy/version'
 require 'gutsy/cli'
 
 module Gutsy
   def self.initialize!
-    args = ARGV
-
-    command = args[0]
-
-    case command
-    when "generate"
-      unless args.length == 4
-        puts <<-TEXT
-Error: Not enough arguments for command 'generate'
-
-Usage: gutsy generate [app_name] [schema_path] [output_path]
-
-DESCRIPTION
-    Generates a gem scaffold and resource API clients on top of a heroics-generated client.
-
-ARGUMENTS
-    [app_name]    - CamelCased name of your application
-    [schema_path] - Path to your JSON Schema file
-    [output_path] - Path to output generated API client gem.
-                    (Will be created if it doesn't exist)
-TEXT
-        exit 1
-      end
-      app_name = args[1]
-      schema_path = File.expand_path(args[2])
-      output_path = File.expand_path(args[3])
-
-      generator = Gutsy::Cli::Generator.new(app_name, schema_path, output_path)
-
-      begin
-        generator.generate!
-      rescue => e
-        puts "FAIL"
-        puts e.message
-        puts e.backtrace.join("\n")
-        exit 1
-      end
-
-      exit 0
-    when "version"
-      puts "Gutsy version #{Gutsy::VERSION}"
-      exit 0
-    else
-      puts <<-TEXT
-Usage: gutsy [command] [options]
-
-DESCRIPTION
-    Generates gem wrappers around heroics-generated API clients
-    built with JSON Schema. (Enough layers of generation for ya?)
-
-COMMANDS
-    generate      scaffolds out an API client
-    version       returns the gutsy version
-    help          displays this message
-
-Shouts out Mr. Gutsy. Keep on plugging in the Wasteland.
-      TEXT
-      exit 0
-    end
+    Gutsy::Cli.parse!(ARGV)
   end
 end
--- lib/gutsy/cli.rb
+++ lib/gutsy/cli.rb
@@ -1,7 +1,80 @@
+require 'gutsy/generator'
+require 'gutsy/version'
+
 module Gutsy
   module Cli
-    # Perhaps one day there will be code here
+    def self.parse!(args)
+      command = args[0]
+
+      case command
+      when "generate"
+        generate(args[1..-1])
+      when "version"
+        version
+      else
+        help
+      end
+    end
+
+    def self.generate(args)
+      unless args.length >= 3
+        puts <<-TEXT
+Error: Not enough arguments for command 'generate'
+
+Usage: gutsy generate [app_name] [schema_path] [output_path] [version]
+
+DESCRIPTION
+    Generates a gem scaffold and resource API clients on top of a heroics-generated client.
+
+ARGUMENTS
+    [app_name]    - CamelCased name of your application
+    [schema_path] - Path to your JSON Schema file
+    [output_path] - Path to output generated API client gem.
+                    (Will be created if it doesn't exist)
+    [version]     - API version to use in client (Default: v1)
+TEXT
+        exit 1
+      end
+      app_name = args[0]
+      schema_path = File.expand_path(args[1])
+      output_path = File.expand_path(args[2])
+      version = args[3] || 'v1'
+
+      generator = Gutsy::Cli::Generator.new(app_name, schema_path, output_path, [version])
+
+      begin
+        generator.generate!
+      rescue => e
+        puts "FAIL"
+        puts e.message
+        puts e.backtrace.join("\n")
+        exit 1
+      end
+
+      exit 0
+    end
+
+    def self.version
+      puts "Gutsy version #{Gutsy::VERSION}"
+      exit 0
+    end
+
+    def self.help
+      puts <<-TEXT
+Usage: gutsy [command] [options]
+
+DESCRIPTION
+    Generates gem wrappers around heroics-generated API clients
+    built with JSON Schema. (Enough layers of generation for ya?)
+
+COMMANDS
+    generate      scaffolds out an API client
+    version       returns the gutsy version
+    help          displays this message
+
+Shouts out Mr. Gutsy. Keep on plugging in the Wasteland.
+      TEXT
+      exit 0
+    end
   end
 end
-
-require 'gutsy/generator'
